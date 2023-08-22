68ad74599947d41a98fe3c4ad2f8b5ffa95a1c38
azamdzobair <azamdzobair@gmail.com>
2023-08-22 13:03:04 +0600

    add Features

--- src/commonent/Features/Features.js
+++ src/commonent/Features/Features.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Title from '../Titlle/Title';
+import Card from './Card';
+import { CardData } from '../Data/CardData';
+
+
 
 const Features = () => {
     return (
@@ -6,8 +11,13 @@ const Features = () => {
             id="features"
             className="w-full py-20 border-b-[1px] border-b-black"
         >
-            <h1>Hello</h1>
+            <Title title="Features" des="What I Do" ></Title>
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
+                {CardData.map((item) => (
+                    <Card item={item} key={item.id}></Card>
+                ))}
 
+            </div>
         </section>
     );
 };
